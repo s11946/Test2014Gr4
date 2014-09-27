7a55e35a0b1800cdc09227d6f963b8504298dd0f
s11946 <s11946@lab5l-16.pjwstk.edu.pl>
2014-09-27 17:55:41 +0200

    blablabla

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Sir");
+		System.out.println("Hello Sir");
+		System.out.println("Hello Sir");
+		System.out.println("Hello Sir");
+		System.out.println("Hello Sir");
+		
 
 	}
 
